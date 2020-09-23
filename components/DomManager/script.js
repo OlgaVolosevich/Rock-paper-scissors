e159315b9e4e7515c5dfbcdb943328f84c8af443
Olga Volosevich <olga.lucky.v@gmail.com>
2020-09-23 23:49:01 +0300

    styles: add basic css styles

--- components/DomManager/script.js
+++ components/DomManager/script.js
@@ -4,39 +4,39 @@ class DomManager {
     document.getElementsByClassName("options__item-wrapper")
   );
   _root = document.getElementsByClassName("results")[0];
-  _resultsContainer = document.getElementsByClassName("results__container")[0];
-  _closeModalBtn = document.getElementsByClassName("icon-close")[0];
+  _resultsAlert = document.getElementsByClassName("results__alert")[0];
+  _results = {
+    user: document.getElementsByClassName("user-result")[0],
+    computer: document.getElementsByClassName("comp-result")[0],
+  };
   _winAlert = `You won!`;
   _loseAlert = `Sorry, you lost`;
   _drawAlert = `It is a draw!`;
   _variants = {
-    scissors: `<div class="results__item-wrapper"><i class="results__item icon-hand-scissors-o"></i></div>`,
-    rock: `<div class="results__item-wrapper"><i class="results__item icon-hand-rock-o"></i></div>`,
-    paper: `<div class="results__item-wrapper"><i class="results__item icon-hand-grab-o"></i></div>`,
+    scissors: `<i class="results__item icon-hand-scissors-o"></i>`,
+    rock: `<i class="options__item icon-hand-grab-o">`,
+    paper: `<i class="results__item icon-hand-rock-o"></i>`,
   };
   cleanResults() {
-    return (this._resultsContainer.innerHTML = "");
+    for (let el in this._results) {
+      this._results[el].innerHTML = ""
+    }
+    return;
   }
   showAlert(result) {
     const ALERT = `_${result}Alert`;
-    const htmlElement = `<p class="results__alert">${this[ALERT]}</p>`;
-    return (this._resultsContainer.innerHTML += htmlElement);
+    return (this._resultsAlert.innerHTML = `${this[ALERT]}`);
   }
   getOptions() {
     return this._options;
   }
-  getCloseBtn() {
-    return this._closeModalBtn;
-  }
-  
+
   showResults(result, userChoice, computerChoice) {
     this.cleanResults();
     this.showAlert(result);
-      const RESULTS = `
-        User: ${this._variants[userChoice]}
-        Computer: ${this._variants[computerChoice]}
-    `;
-      return (this._resultsContainer.innerHTML += RESULTS);
+    this._results.user.innerHTML = `${this._variants[userChoice]}`;
+    this._results.computer.innerHTML = `${this._variants[computerChoice]}`;
+    return;
   }
 }
 
